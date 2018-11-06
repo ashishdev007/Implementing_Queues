c0767ddedd06864ecb2dbab3d81a8fe7d0636272
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-05 23:17:02 -0600

    "Implemented QueueInt in a LinkedQueue class"

---
+++ src/LinkedQueue.java
@@ -0,0 +1,22 @@
+public class LinkedQueue<E> implements QueueInt<E> {
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E offer() {
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+}
